8eeb0fc6c2d36f00f5280de3fde83076eefe4946
Francisco Viana <franciscoviana862@gmail.com>
2020-02-01 21:53:40 -0300

    created: component add-task;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AddTaskComponent } from './pages/add-task/add-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
